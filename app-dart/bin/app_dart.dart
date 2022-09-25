2b39476f858c9df2426c6ac93695f0ef379eb901
hambaallah14 <fahmiazri11@gmail.com>
2022-09-25 23:57:02 +0700

    Membuat aplikasi dart Hello World

---
+++ app-dart/bin/app_dart.dart
@@ -0,0 +1,5 @@
+import 'package:app_dart/app_dart.dart' as app_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${app_dart.calculate()}!');
+}
---
+++ app-dart/bin/hello_world.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 3;
+  double b = 4.5;
+  var jumlah = a+b;
+  String nama = "Hello World";
+  
+  for (var i = 0; i < 10; i++) {
+    print(jumlah);
+  }
+}
